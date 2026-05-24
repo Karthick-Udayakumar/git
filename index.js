e0e6a61d78a2e0ead3a14cab9371cee75cd42e74
Karthick Udayakumar <karthickudayakumar@outlook.com>
2026-05-24 22:55:21 +0530

    Checkin index.html and index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('This is a sample file')
+console.log('Welcome to GitHub Playground!');
+
+for(let i = 0; i < 5; i++) {
+    console.log(`Iteration ${i}`);
+}
+
+console.log('This is a simple JavaScript file to demonstrate GitHub Playground.');
